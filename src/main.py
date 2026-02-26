ef8e154123c13a2e6bcd0021f5b81a55453459d8
dominikjakobsze <dominikjakobsze00@gmail.com>
2026-02-26 11:16:12 +0100

    refactor: Implement custom recursive directory copy

--- src/main.py
+++ src/main.py
@@ -18,7 +18,13 @@ def _delete_files_in_dir(target: Path):
 
 
 def _copy_file_to_dir(src: Path, dst: Path):
-    shutil.copytree(src, dst, dirs_exist_ok=True)
+    # shutil.copytree(src, dst, dirs_exist_ok=True)
+    for file in src.iterdir():
+        if file.is_file():
+            shutil.copy2(file, dst)
+        if file.is_dir():
+            (dst / file.name).resolve().mkdir(parents=True, exist_ok=True)
+            _copy_file_to_dir(file, (dst / file.name).resolve())
 
 
 def static_to_public():
@@ -26,10 +32,5 @@ def static_to_public():
     _delete_files_in_dir((ROOT_DIR / "public").resolve())
     _copy_file_to_dir((ROOT_DIR / "static").resolve(), (ROOT_DIR / "public").resolve())
 
-
-# Write a recursive function that copies all the contents from a source directory to a destination directory (in our case, static to public)
-# It should first delete all the contents of the destination directory (public) to ensure that the copy is clean.
-# It should copy all files and subdirectories, nested files, etc.
-# I recommend logging the path of each file you copy, so you can see what's happening as you run and debug your code.
 
 main()
